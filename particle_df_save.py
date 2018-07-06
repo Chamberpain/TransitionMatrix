2d2aa34b3a519fc42bea4b12ecf216d20d195631
Paul Chamberlain <pchamberlain@Pauls-MacBook-Pro-2.local>
2018-07-06 14:24:10 -0700

    added depth parsing

--- particle_df_save.py
+++ particle_df_save.py
@@ -17,15 +17,24 @@ Depth = mat['Depth']
 
 opt=np.fromfile(data_file_name,'>f4').reshape(-1,3,npts)
 
-# print "data has %i records" %(opt.shape[0])
+print "data has %i records" %(opt.shape[0])
 
 
-#plot some trajectories
-x,y=opt[:,0,:10],opt[:,1,:10] #this is in model grid index coordinate, convert to lat-lon using x=x/6.0;y=y/6.0-77.875
+frames = []
+for k in range(opt.shape[2]):
+	x,y=opt[:,0,k],opt[:,1,k]#this is in model grid index coordinate, convert to lat-lon using x=x/6.0;y=y/6.0-77.875
+	t = range(opt.shape[0])
+	x=x/6.0;y=y/6.0-77.875
+	x = x%360
+	frames.append(pd.DataFrame({'Lat':y,'Lon':x,'Date':t}))
+	frames[-1]['Cruise']=k
+df = pd.concat(frames)
 
-x=x/6.0;y=y/6.0-77.875
-x = x%360
-plt.plot(x,y,'-')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+####### this code is necessary because Isa accidentally deployed the particles on land ####
+df['bins_lat'] = pd.cut(df.Lat,bins=YC,labels=YC[:-1])
+df['bins_lon'] = pd.cut(df.Lon,bins=XC,labels=XC[:-1])
+
+for i,lon in enumerate(XC[:-1]):
+	print 'lon = ',lon
+	for j,lat in enumerate(YC[:-1]):
+		df.loc[(df.bins_lat==lat)&(df.bins_lon==lon),'Depth']=Depth[i,j]
