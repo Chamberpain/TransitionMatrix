d50e0c29c254fef7d5a899e34d6586152acd4f08
Paul Chamberlain <pchamberlain@Pauls-MacBook-Pro-2.local>
2018-07-09 14:40:02 -0700

    fixed output position type

--- particle_df_save.py
+++ particle_df_save.py
@@ -1,26 +1,17 @@
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.io
+
 
 omega = 7.292*10**-5
 
 data_file_name = os.getenv("HOME")+'/iCloud/Data/Raw/SOSE/particle_release/SO_RAND_0001.XYZ.0000000001.0003153601.data'
-grid_file_name = os.getenv("HOME")+'/iCloud/Data/Raw/SOSE/grid.mat'
-output_file_name = os.getenv("HOME")+'/iCloud/Data/Processed/transition_matrix/sose_particle_df.pickle'
+df_output_file_name = os.getenv("HOME")+'/iCloud/Data/Processed/transition_matrix/sose_particle_df.pickle'
 npts= 10000
-
-
-mat = scipy.io.loadmat(grid_file_name)
-XC = mat['XC'][:,0]
-YC = mat['YC'][0,:]
-Depth = mat['Depth']
 
 opt=np.fromfile(data_file_name,'>f4').reshape(-1,3,npts)
 
 print "data has %i records" %(opt.shape[0])
-
 
 frames = []
 for k in range(opt.shape[2]):
@@ -31,21 +22,5 @@ for k in range(opt.shape[2]):
 	frames.append(pd.DataFrame({'Lat':y,'Lon':x,'Date':t}))
 	frames[-1]['Cruise']=k
 df = pd.concat(frames)
-
-####### this code is necessary because Isa accidentally deployed the particles on land ####
-df['bins_lat'] = pd.cut(df.Lat,bins=YC,labels=YC[:-1])
-df['bins_lon'] = pd.cut(df.Lon,bins=XC,labels=XC[:-1])
-
-for i,lon in enumerate(XC[:-1]):
-	print 'lon = ',lon
-	for j,lat in enumerate(YC[:-1]):
-		df.loc[(df.bins_lat==lat)&(df.bins_lon==lon),'Depth']=Depth[i,j]
-df['PV']=np.sin(np.deg2rad(df.Lat))*omega*2/df.Depth
-frames = []
-for cruise in df.Cruise.unique():
-	print 'we are on cruise ',cruise
-	df_holder = df[df.Cruise==cruise]
-	df_holder['PV Diff'] = df_holder.PV.diff()
-	frames.append(df_holder)
-df = pd.concat(frames)
-df.to_pickle(output_file_name)
+df['position type'] = 'SOSE'
+df.to_pickle(df_output_file_name)
