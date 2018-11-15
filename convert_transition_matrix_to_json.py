2fe6d90e0f0820f743d1e9937580e198692a9088
Paul Chamberlain <pchamber@ucsd.edu>
2018-11-15 15:22:42 -0800

    first commit of transition matrix to json translator

---
+++ convert_transition_matrix_to_json.py
@@ -0,0 +1,25 @@
+from transition_matrix_compute import argo_traj_data
+import json 
+
+for span_limit in [60,120]:
+	traj_class = argo_traj_data(degree_bins=2,date_span_limit=span_limit)
+	for column in range(traj_class.transition_matrix.shape[0]):
+		print 'there are ',(traj_class.transition_matrix.shape[0]-column),'columns remaining'
+		p_lat,p_lon = tuple(traj_class.total_list[column])
+		data = traj_class.transition_matrix[:,column].data
+		lat,lon = zip(*[tuple(traj_class.total_list[x]) for x in traj_class.transition_matrix[:,0].indices.tolist()])
+		feature_list = zip(lat,lon,data)
+		geojson = {'type':'FeatureCollection', 'features':[]}
+		for token in feature_list:
+			lat,lon,prob = token
+			feature = {'type':'Feature',
+						'properties':{},
+						'geometry':{'type':'Point',
+						'coordinates':[]}}
+			feature['geometry']['coordinates'] = [lat,lon]
+			feature['properties']['Probability'] = prob
+			geojson['features'].append(feature)
+		output_filename = './'+str(span_limit)+'_day/'+str(span_limit)+'_lat_'+str(p_lat)+'_lon_'+str(p_lon)+'.js'
+		with open(output_filename,'wb') as output_file:
+			output_file.write('var dataset = ')
+			json.dump(geojson, output_file, indent=2) 
