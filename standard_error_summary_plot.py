e72205fda0da77776b01723f64ab6570ec04b48f
Paul Chamberlain <pchamber@ucsd.edu>
2019-01-18 15:20:51 +0000

    Added displacement number plots and ability to load and save previous data

--- standard_error_summary_plot.py
+++ standard_error_summary_plot.py
@@ -5,53 +5,80 @@ import scipy.sparse
 
 
 base_file = os.getenv("HOME")+'/iCloud/Data/Processed/transition_matrix/'
-degree_bin_list = [(2,3),(3,6),(1,1),(2,2),(3,3),(4,4)]
 time_list = [20,40,60,80,100,120,140,160,180]
-num_list = []
-std_list = []
-mean_list = []
 
 
-for degree_bin in degree_bin_list:
-        print degree_bin 
-        lat_bin,lon_bin = degree_bin
-        num_list_holder = []
-        mean_list_holder = []
-        std_list_holder = []
-        for date_span_limit in time_list:
-                print date_span_limit
-                filename = base_file+'transition_matrix/standard_error_matrix_degree_bins_'+str(degree_bin)+'_time_step_'+str(date_span_limit)+'.npz'
-                loader = np.load(filename)
-                data = loader['data']
+try:
+    datalist = np.load(base_file+'transition_matrix/data_summary_list')
+except IOError:
+    total_cell_num_list = []
+    error_std_list = []
+    error_mean_list = []
+    displacements_per_cell_std_list = []
+    displacements_per_cell_mean_list = []
+    degree_bin_list = [(2,3),(3,6),(1,1),(2,2),(3,3),(4,4)]
+    for degree_bin in degree_bin_list:
+            print degree_bin 
+            lat_bin,lon_bin = degree_bin
+            total_cell_num_list_holder = []
+            error_mean_list_holder = []
+            error_std_list_holder = []
+            displacements_per_cell_std_list_holder = []
+            displacements_per_cell_mean_list_holder = []
+            for date_span_limit in time_list:
+                    print date_span_limit
+                    filename = base_file+'transition_matrix/standard_error_matrix_degree_bins_'+str(degree_bin)+'_time_step_'+str(date_span_limit)+'.npz'
+                    loader = np.load(filename)
+                    data = loader['data']
+                    data[np.isinf(data)]=np.nan
+                    matrix = scipy.sparse.csr_matrix((data, loader['indices'], loader['indptr']),
+                          shape=loader['shape']).T
+                    mean = np.nanmean(data)
+                    error_mean_list_holder.append(mean)
+                    std = np.nanstd(data)
+                    error_std_list_holder.append(std)
+                    num = len(np.diag(matrix.todense())[~np.isnan(np.diag(matrix.todense()))])
+                    total_cell_num_list_holder.append(num)
 
-                data[np.isinf(data)]=np.nan
+                    filename = base_file+'transition_matrix/number_matrix_degree_bins_'+str(degree_bin)+'_time_step_'+str(date_span_limit)+'.npz'
+                    loader = np.load(filename)
+                    data = loader['data']
+                    mean = np.mean(data)
+                    displacements_per_cell_mean_list_holder.append(mean)
+                    displacements_per_cell_std_list_holder.append(np.std(data))
 
-                matrix = scipy.sparse.csr_matrix((data, loader['indices'], loader['indptr']),
-                      shape=loader['shape']).T
-                mean = np.nanmean(data)
-                mean_list_holder.append(mean)
-                std = np.nanstd(data)
-                std_list_holder.append(std)
-                num = len(np.diag(matrix.todense())[~np.isnan(np.diag(matrix.todense()))])
-                num_list_holder.append(num)
-        num_list.append(num_list_holder)
-        std_list.append(std_list_holder)
-        mean_list.append(mean_list_holder)
+            total_cell_num_list.append(total_cell_num_list_holder)
+            error_std_list.append(error_std_list_holder)
+            error_mean_list.append(error_mean_list_holder)
+            displacements_per_cell_std_list.append(displacements_per_cell_std_list_holder)
+            displacements_per_cell_mean_list.append(displacements_per_cell_mean_list_holder)
+    datalist = zip(total_cell_num_list,error_std_list,error_mean_list,displacements_per_cell_std_list,displacements_per_cell_mean_list,degree_bin_list)
+    np.save(base_file+'transition_matrix/data_summary_list',datalist)
 
-for token in zip(num_list,std_list,mean_list,degree_bin_list):
-        num_list_holder,std_list_holder,mean_list_holder,name = token
+for token in datalist:
+        total_cell_num_list_holder,error_std_list_holder,error_mean_list_holder,displacements_per_cell_std_list_holder,displacements_per_cell_mean_list_holder,name = token
         plt.figure('number plot')
-        plt.plot(num_list_holder,'o-',label=str(name))
+        plt.plot(time_list,total_cell_num_list_holder,'o-',label=str(name))
         plt.figure('standard error')
-        plt.errorbar(time_list,mean_list_holder,yerr=np.array(std_list_holder),fmt='o-',label=str(name))
+        plt.errorbar(time_list,error_mean_list_holder,yerr=np.array(error_std_list_holder),fmt='o-',alpha = 0.4,label=str(name))
+        plt.figure('displacement number plot')
+        plt.errorbar(time_list,displacements_per_cell_mean_list_holder,yerr=np.array(displacements_per_cell_std_list_holder),fmt='o-',alpha = 0.4,label=str(name))
+
+
 plt.figure('number plot')
 plt.title('Number of non-zero grid cells for each grid resolution and datespan')
 plt.xlabel('Time Step (days)')
 plt.legend()
 plt.ylabel('Number of total grid cells')
+plt.yscale('Log')
 plt.figure('standard error')
 plt.title('Mean and standard deviation of standard error \n for each grid resolution and datespan')
 plt.legend()
 plt.ylabel('Mean standard error')
+plt.xlabel('Time Step (days)')
+plt.figure('displacement number plot')
+plt.title('Mean and standard deviation of displacements per \n cell for each grid resolution and datespan')
+plt.legend()
+plt.ylabel('Displacements per cell')
 plt.xlabel('Time Step (days)')
 plt.show()
